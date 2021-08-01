b5dbb72959f7cb5940c1ff3076615785a1fb4cc2
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-08-01 16:54:26 +0200

    game night poster final edits

--- R/2021-08-06.R
+++ R/2021-08-06.R
@@ -3,6 +3,7 @@ social(
   output = "posters/2021-08-06.png",
   rmd_params = list(
     number = "#1",
-    date = 'Vendredi 6 Août 2021 à 19 h 30'
+    date = "Vendredi 6 Août 2021 à 19 h 30",
+    event = "https://fb.me/e/2BEUnTzJG"
   )
 )
--- R/social.R
+++ R/social.R
@@ -4,8 +4,6 @@ library(callr)
 social <- function(input, output, rmd_params, chrome_path, delay = 1) {
   callr::r(
     func = function(input, output, rmd_params, chrome_path, delay) {
-      output_file <- file.path("ads", sub("\\.png$", ".pdf", basename(output)))
-
       web_browser <- suppressMessages(try(chromote::ChromoteSession$new(), silent = TRUE))
 
       if (
@@ -29,21 +27,9 @@ social <- function(input, output, rmd_params, chrome_path, delay = 1) {
         params = rmd_params
       )
 
-      # file.copy(
-      #   from = xaringan_poster,
-      #   to = file.path("ads", sub("\\.png$", ".html", basename(output))),
-      #   overwrite = TRUE
-      # )
-
       web_browser$Page$navigate(xaringan_poster, wait_ = FALSE)
       on.exit(web_browser$close(), add = TRUE)
       web_browser$Page$loadEventFired()
-
-      web_browser$screenshot(
-        filename = output,
-        selector = "div.remark-slide-scaler",
-        scale = 2
-      )
 
       current_slide <- function() {
         x <- web_browser$Runtime$evaluate("slideshow.getCurrentSlideIndex()")$result$value
@@ -62,43 +48,16 @@ social <- function(input, output, rmd_params, chrome_path, delay = 1) {
         )$result$value)
       }
 
-      slide_size <- ({
-        r <- web_browser$Runtime$evaluate('slideshow.getRatio()')$result$value
-        r <- lapply(strsplit(r, ":"), as.integer)
-        width <- r[[1]][1]
-        height <- r[[1]][2]
-        page_width <- 8/width * width
-        list(
-          width = as.integer(908 * width / height),
-          height = 681L,
-          page = list(width = page_width, height = page_width * height / width)
-        )
-      })
-
       expected_slides <- as.integer(
         web_browser$Runtime$evaluate("slideshow.getSlideCount()")$result$value
       )
 
       max_slides <- expected_slides * 4
 
-      web_browser$Browser$setWindowBounds(1, bounds = list(
-        width = slide_size$width,
-        height = slide_size$height
-      ))
-
-      web_browser$Emulation$setEmulatedMedia("print")
-      web_browser$Runtime$evaluate(paste0(
-        "let style = document.createElement('style')\n",
-        "style.innerText = '@media print { ",
-        ".remark-slide-container:not(.remark-visible){ display:none; }",
-        "}'\n",
-        "document.head.appendChild(style)"
-      ))
-
       idx_slide <- current_slide()
       last_hash <- ""
       idx_part <- 0L
-      pdf_files <- c()
+      # png_files <- vector("character", max_slides)
       for (i in seq_len(max_slides)) {
         if (i > 1) {
           web_browser$Input$dispatchKeyEvent(
@@ -126,30 +85,12 @@ social <- function(input, output, rmd_params, chrome_path, delay = 1) {
         if (identical(last_hash, this_hash)) break
         last_hash <- this_hash
 
-        pdf_file_promise <- web_browser$Page$printToPDF(
-          landscape = TRUE,
-          printBackground = TRUE,
-          paperWidth = 12,
-          paperHeight = 9,
-          marginTop = 0,
-          marginRight = 0,
-          marginBottom = 0,
-          marginLeft = 0,
-          pageRanges = "1",
-          preferCSSPageSize = TRUE,
-          wait_ = FALSE
-        )$then(function(value) {
-          filename <- tempfile(fileext = ".pdf")
-          writeBin(jsonlite::base64_dec(value$data), filename)
-          filename
-        })
-        pdf_files <- c(pdf_files, web_browser$wait_for(pdf_file_promise))
+        web_browser$screenshot(
+          filename = sprintf(sub("\\.png", "_%02d.png", output), i),
+          selector = "div.remark-slide-scaler",
+          scale = 2
+        )
       }
-
-      pdftools::pdf_combine(pdf_files, output = output_file)
-      unlink(pdf_files)
-
-      invisible(output_file)
     },
     args = list(
       input = input,
