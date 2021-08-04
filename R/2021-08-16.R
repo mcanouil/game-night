f27ae6a3807722fe2b2099396fdeb45ce822fde5
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-08-04 18:21:21 +0200

    update event url

--- R/2021-08-16.R
+++ R/2021-08-16.R
@@ -4,7 +4,7 @@ social(
   rmd_params = list(
     title = "<h1 style = 'font-size:100px;'>C'Est L'Histoire D'Un-e Nain-e ...</br>#27<h1>",
     date = "Lundi 16 Août 2021 à 19 h 30",
-    event = "https://lille.onvasortir.com/",
+    event = "https://lille.onvasortir.com/c-039est-l-039histoire-d-039un-e-nain-e-27-5583451.html",
     group = "On Va Sortir (OVS)"
   )
 )
