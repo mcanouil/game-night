7424e58bd136a3cd4e9e8d0faf9d14399a7afa2f
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-08-21 11:08:40 +0200

    new GN OVS

---
+++ R/2021-08-30.R
@@ -0,0 +1,10 @@
+social(
+  input = "poster.Rmd",
+  output = "posters/2021-08-30.png",
+  rmd_params = list(
+    title = "<h1 style = 'font-size:100px;'>C'Est L'Histoire D'Un-e Nain-e ...</br>#28<h1>",
+    date = "Lundi 30 Août 2021 à 19 h 30",
+    event = "https://lille.onvasortir.com/c-039est-l-039histoire-d-039un-e-nain-e-28-5595731.html",
+    group = "On Va Sortir (OVS)"
+  )
+)
