7c86d966c6fb0e75da0af754694f14ac6fc67b60
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-09-18 12:27:07 +0200

    clean past events

---
+++ R/2021-09-24.R
@@ -0,0 +1,9 @@
+social(
+  input = "poster.Rmd",
+  output = "posters/2021-09-24.png",
+  rmd_params = list(
+    title = "Game Night #5",
+    date = "Vendredi 24 Septembre 2021 à 19 h 30",
+    group = "Les Sorties Lilloises (LSL)"
+  )
+)
