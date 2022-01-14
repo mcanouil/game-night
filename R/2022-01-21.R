cd24c36953d9c16b2571aef7029c9c0ad8aa5d20
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-01-14 20:42:54 +0100

    new GN

---
+++ R/2022-01-21.R
@@ -0,0 +1,9 @@
+social(
+  input = "poster.Rmd",
+  output = "posters/2022-01-21.png",
+  rmd_params = list(
+    title = "Game Night #9",
+    date = "Vendredi 21 Janvier 2022 à 19 h 30",
+    group = "Les Sorties Lilloises (LSL)"
+  )
+)
