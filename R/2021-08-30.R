43418052a249d35beb94f34e24d2f6c940550638
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-01-15 13:10:04 +0100

    reset previous poster

--- R/2021-08-30.R
+++
@@ -1,8 +0,0 @@
-social(
-  output = "posters/2021-08-30.png",
-  rmd_params = list(
-    title = "<h1 style = 'font-size:100px;'>C'Est L'Histoire D'Un-e Nain-e ...</br>#28<h1>",
-    date = "Lundi 30 Août 2021 à 19 h 30",
-    group = "On Va Sortir (OVS)"
-  )
-)
