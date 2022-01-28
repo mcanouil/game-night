63f5f7f236df2506f2c0e907397a1f827e7f389a
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-01-28 20:24:48 +0100

    new game night

---
+++ R/2022-02-04.R
@@ -0,0 +1,8 @@
+social(
+  output = "posters/2022-02-04.png",
+  rmd_params = list(
+    title = "Game Night #10",
+    date = "Vendredi 4 Février 2022 à 19 h 30",
+    group = "Les Sorties Lilloises (LSL)"
+  )
+)
