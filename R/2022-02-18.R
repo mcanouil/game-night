d14f84130fbc7aaacb2aa56d3982bb2d16bb7f91
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-02-06 16:15:28 +0100

    fix(2022-02-18): wrong settings

--- R/2022-02-18.R
+++ R/2022-02-18.R
@@ -1,8 +1,8 @@
 social(
-  output = "posters/2022-02-11.png",
+  output = "posters/2022-02-18.png",
   rmd_params = list(
     title = "Game Night #11",
-    date = "Vendredi 11 Février 2022 à 19 h 30",
+    date = "Vendredi 18 Février 2022 à 19 h 30",
     group = "Les Sorties Lilloises (LSL)"
   )
 )
