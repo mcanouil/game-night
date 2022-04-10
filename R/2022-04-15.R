36b5238b593e764e75b414f113d4c18c0effd797
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-04-10 18:01:23 +0200

    fix: date

--- R/2022-04-15.R
+++ R/2022-04-15.R
@@ -1,5 +1,5 @@
 create_game_night(
-  output = "posters/2022-04-015.png",
+  output = "posters/2022-04-15.png",
   rmd_params = list(
     number = "14",
     date = "Vendredi 15 Avril 2022 à 19 h 30"
