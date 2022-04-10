65df876fd32839bd032b519a46dc9e43e79baf61
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-04-10 17:45:23 +0200

    docs: set next gn

--- R/2022-04-08.R
+++
@@ -1,7 +0,0 @@
-create_game_night(
-  output = "posters/2022-04-08.png",
-  rmd_params = list(
-    number = "14",
-    date = "Vendredi 8 Avril 2022 à 19 h 30"
-  )
-)
---
+++ R/2022-04-15.R
@@ -0,0 +1,7 @@
+create_game_night(
+  output = "posters/2022-04-015.png",
+  rmd_params = list(
+    number = "14",
+    date = "Vendredi 15 Avril 2022 à 19 h 30"
+  )
+)
